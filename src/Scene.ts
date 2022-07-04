2b0fe1e955ba1d60bb41242de58c0a3b459b6cae
JuanAmaral <trabalho@juanamaral.com.br>
2022-07-04 16:28:55 -0300

    animation example pixi

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,6 +1,7 @@
 import { AnimatedSprite, Container, Texture } from "pixi.js";
 
 export class Scene extends Container {
+
     constructor() {
         console.log("chamando animação");
         super();
@@ -11,27 +12,38 @@ export class Scene extends Container {
         //   "clampy_sequence_03.png",
         //   "clampy_sequence_04.png"
         // ];
-        let camp_images = ["rav.png","rav2.png","clampy.png","rav2.png"];
+        let camp_images = 
+        [
+            "http://localhost:1234/1.png",
+            "http://localhost:1234/2.png",
+            "http://localhost:1234/3.png"
+        ];
         let textureArray = [];
         // `array.map()` creates an array from another array by doing something to each element.
         // `(stringy) => Texture.from(stringy)` means
         // "A function that takes a string and returns a Texture.from(that String)"
         //const animatedClampy: AnimatedSprite = new AnimatedSprite(clampyFrames.map((stringy) => Texture.from(stringy)));
-        for (let i=0; i < 4; i++)
+        for (let i=0; i < 3; i++)
         {
             let texture = Texture.from(camp_images[i]);
             textureArray.push(texture);
             console.log("carreguei: " +  i);
         };
+        console.log(camp_images)
+        console.log(textureArray)
         // (if this javascript is too much, you can do a simple for loop and create a new array with Texture.from())
-        let animatedSprite = new AnimatedSprite(textureArray);
+        const animatedClampy: AnimatedSprite = new AnimatedSprite(camp_images.map((stringy) => Texture.from(stringy)));
+        // (if this javascript is too much, you can do a simple for loop and create a new array with Texture.from())
 
-        this.addChild(animatedSprite); // we just add it to the scene
+        this.addChild(animatedClampy); // we just add it to the scene
         // Now... what did we learn about assigning functions...
-        animatedSprite.onFrameChange = this.onClampyFrameChange.bind(this);
+        animatedClampy.onFrameChange = this.onClampyFrameChange.bind(this);
+        animatedClampy.animationSpeed = 1/30; 
+        animatedClampy.play();
     }
 
     private onClampyFrameChange(currentFrame : any): void {
         console.log("Clampy's current frame is", currentFrame);
     }
+  
 }
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,56 @@
-import { Application, Sprite, Container  } from 'pixi.js'
-import { ShockwaveFilter, GlowFilter } from 'pixi-filters'
+import { Application  } from 'pixi.js'
+// import { ShockwaveFilter, GlowFilter } from 'pixi-filters'
 import { Scene } from './Scene';
 const app = new Application({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
     resolution: window.devicePixelRatio || 1,
-  autoDensity: true,
+    autoDensity: true,
     backgroundColor: 0x6495ed,
-    width: 800,
-    height: 500
+    width: 640,
+    height: 480
 });
+// pass in the screen size to avoid "asking up"
+const sceny: Scene = new Scene();
+app.stage.addChild(sceny)
 
-const conty: Container = new Container();
-conty.x = 0;
-conty.y = 0;
-app.stage.addChild(conty);
+// const conty: Container = new Container();
+// conty.x = 0;
+// conty.y = 0;
+// app.stage.addChild(conty);
 
-const clampy: Sprite = Sprite.from("rav2.png");
-clampy.x = 0;
-clampy.y = 0;
-conty.addChild(clampy);
+// const clampy: Sprite = Sprite.from("rav2.png");
+// clampy.x = 0;
+// clampy.y = 0;
+// conty.addChild(clampy);
 
-const clampy2: Sprite = Sprite.from("rav2.png");
-clampy2.anchor.set(0.5);
-clampy2.x = app.screen.width /2 ;
-clampy2.y = app.screen.height /2 ;
-conty.addChild(clampy2);
+// const clampy2: Sprite = Sprite.from("rav2.png");
+// clampy2.anchor.set(0.5);
+// clampy2.x = app.screen.width /2 ;
+// clampy2.y = app.screen.height /2 ;
+// conty.addChild(clampy2);
 
-const clampy3: Sprite = Sprite.from("rav2.png");
-clampy3.anchor.set(0.5);
-clampy3.x = 200 ;
-clampy3.y = 50;
-conty.addChild(clampy3);
+// const clampy3: Sprite = Sprite.from("rav2.png");
+// clampy3.anchor.set(0.5);
+// clampy3.x = 200 ;
+// clampy3.y = 50;
+// conty.addChild(clampy3);
+
+// // const myBlurFilter = new GlowFilter();
+// // myBlurFilter.enabled = true;
+// // myBlurFilter.innerStrength = 5;
+// // clampy.filters = [myBlurFilter];
+
+// const myShockwaveFilter = new ShockwaveFilter();
+// myShockwaveFilter.enabled = true;
+// myShockwaveFilter.padding = 1;
+
+// clampy.filters = [myShockwaveFilter];
+
 
 // const myBlurFilter = new GlowFilter();
 // myBlurFilter.enabled = true;
-// myBlurFilter.innerStrength = 5;
-// clampy.filters = [myBlurFilter];
-
-const myShockwaveFilter = new ShockwaveFilter();
-myShockwaveFilter.enabled = true;
-myShockwaveFilter.padding = 1;
-
-clampy.filters = [myShockwaveFilter];
-
-
-const myBlurFilter = new GlowFilter();
-myBlurFilter.enabled = true;
-myBlurFilter.color = 1
-clampy2.filters = [myBlurFilter];
+// myBlurFilter.color = 1
+// clampy2.filters = [myBlurFilter];
 
 
 // const sceny: Scene = new Scene(app.screen.width, app.screen.height);
